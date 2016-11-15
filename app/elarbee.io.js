3c5f7d163b44a51a300af2e3f670a3ebb22a5263
elarbee <aelarbee@gmail.com>
2016-11-15 14:24:27 -0500

    Various stuff and junk.

--- app/elarbee.io.js
+++ app/elarbee.io.js
@@ -1,2 +1,9 @@
 'use strict';
-angular.module('elarbee.io',['core.icon',"iconList"]);
+var elarbee = angular.module('elarbee.io',['core.icon',"iconList"]);
+
+elarbee.controller("elarbeeCtrl", function($scope){
+
+	//Spin class that will be added on mouseEnter
+	
+	$scope.spin = "";
+});
--- app/icon-list/icon-list.component.js
+++ app/icon-list/icon-list.component.js
@@ -5,7 +5,6 @@ angular.
 	      controller: ['Icon',
 			    function IconListController(Icon) {
 					    this.icons = Icon.query();
-		    		    console.log(this.icons);
 					    this.orderProp = 'name';
 					  }
 			  ]
