50ff31f3dffcd39de0346acda2cbc9a07c15a3f0
elarbee <aelarbee@gmail.com>
2016-12-28 16:08:50 -0500

    Added Express server and made ready to deploy to AWS

---
+++ app/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path    = require("path");
+
+app.use(express.static(__dirname + '/static'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.get('/',function(req,res){
+	 res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+var port = 8081;
+app.listen(port, function () {
+	console.log("elarbee.io server started on port "+port);
+});
+
+app.get("/secret", function(req,res){
+	res.send("SECRET!!");
+})
