bba59f8245377b69d270c871e3a619ae5b91e593
Alex Elarbee <alex@Alexs-MacBook-Pro.local>
2018-10-14 19:52:33 -0400

    Add animations to logo.
    Thanks to https://css-tricks.com/svg-line-animation-works/

--- src/app/animations/animations.module.ts
+++ src/app/animations/animations.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AnimationsPageComponent } from './animations-page/animations-page.component';
 import { TestComponent } from './test/test.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FlexLayoutModule
   ],
-  declarations: [AnimationsPageComponent, TestComponent]
+  declarations: [AnimationsPageComponent, TestComponent, LogoComponent]
 })
 export class AnimationsModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,3 +20,4 @@ export const routes: Routes = [
 export class AppRoutingModule {
 
 }
+git
